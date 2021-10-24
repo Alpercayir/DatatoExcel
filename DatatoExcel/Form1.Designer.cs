d311e4a77cf625b2c18768495d31c06c05d8bf15
alpercayir <alpercayir.can@gmail.com>
2021-10-24 16:59:23 +0300

    first commit

---
+++ DatatoExcel/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace DatatoExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtYear = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtModel = new System.Windows.Forms.TextBox();
+            this.TxtBodyType = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtEngineType = new System.Windows.Forms.TextBox();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtStatus = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Year:";
+            // 
+            // TxtYear
+            // 
+            this.TxtYear.Location = new System.Drawing.Point(209, 41);
+            this.TxtYear.Name = "TxtYear";
+            this.TxtYear.Size = new System.Drawing.Size(161, 30);
+            this.TxtYear.TabIndex = 1;
+            this.TxtYear.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Model:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "BodyType:";
+            // 
+            // TxtModel
+            // 
+            this.TxtModel.Location = new System.Drawing.Point(209, 107);
+            this.TxtModel.Name = "TxtModel";
+            this.TxtModel.Size = new System.Drawing.Size(161, 30);
+            this.TxtModel.TabIndex = 4;
+            this.TxtModel.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // TxtBodyType
+            // 
+            this.TxtBodyType.Location = new System.Drawing.Point(209, 166);
+            this.TxtBodyType.Name = "TxtBodyType";
+            this.TxtBodyType.Size = new System.Drawing.Size(161, 30);
+            this.TxtBodyType.TabIndex = 5;
+            this.TxtBodyType.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "EngineType:";
+            // 
+            // TxtEngineType
+            // 
+            this.TxtEngineType.Location = new System.Drawing.Point(209, 227);
+            this.TxtEngineType.Name = "TxtEngineType";
+            this.TxtEngineType.Size = new System.Drawing.Size(161, 30);
+            this.TxtEngineType.TabIndex = 7;
+            this.TxtEngineType.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.AccessibleDescription = "";
+            this.BtnListele.AccessibleName = "";
+            this.BtnListele.Location = new System.Drawing.Point(412, 41);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(117, 30);
+            this.BtnListele.TabIndex = 8;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            this.BtnListele.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(412, 106);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(117, 30);
+            this.BtnKaydet.TabIndex = 9;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Location = new System.Drawing.Point(412, 173);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(117, 30);
+            this.BtnGuncelle.TabIndex = 11;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 313);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(472, 183);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(635, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 22);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Status:";
+            // 
+            // TxtStatus
+            // 
+            this.TxtStatus.Location = new System.Drawing.Point(739, 78);
+            this.TxtStatus.Name = "TxtStatus";
+            this.TxtStatus.Size = new System.Drawing.Size(161, 30);
+            this.TxtStatus.TabIndex = 14;
+            this.TxtStatus.TextChanged += new System.EventHandler(this.TxtStatus_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(621, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(279, 22);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Delete Operation Put \"0\" in Status";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(602, 267);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 229);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(735, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(135, 22);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "SelectaNewCar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1052, 667);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.TxtEngineType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtBodyType);
+            this.Controls.Add(this.TxtModel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtYear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtModel;
+        private System.Windows.Forms.TextBox TxtBodyType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtEngineType;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtStatus;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DatatoExcel/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace DatatoExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection linking = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Hakan\Desktop\Vehicles.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES;'");
+
+        void Data() 
+        {
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * From [Sayfa1$]", linking);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+
+                if (dataGridView1.Rows[i].Cells[4].Value.ToString() == "0")
+                {
+                    dataGridView1.Rows[i].Visible = false;
+                }
+            }
+        }     
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Data();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Data();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            linking.Open();
+            OleDbCommand order = new OleDbCommand("insert into [Sayfa1$] ([Year],[Model],[BodyType],[EngineType],[Status]) values(@p1,@p2,@p3,@p4,@p5)",linking);
+            order.Parameters.AddWithValue("@p1", TxtYear.Text);
+            order.Parameters.AddWithValue("@p2", TxtModel.Text);
+            order.Parameters.AddWithValue("@p3", TxtBodyType.Text);
+            order.Parameters.AddWithValue("@p3", TxtEngineType.Text);
+            order.Parameters.AddWithValue("@p5", TxtStatus.Text);
+            order.ExecuteNonQuery();        
+            linking.Close();
+            MessageBox.Show("Vehicle has been recorded.","inform",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            Data();
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int choosen = dataGridView1.SelectedCells[0].RowIndex;
+            TxtYear.Text = dataGridView1.Rows[choosen].Cells[0].Value.ToString();
+            TxtModel.Text = dataGridView1.Rows[choosen].Cells[1].Value.ToString();
+            TxtBodyType.Text = dataGridView1.Rows[choosen].Cells[2].Value.ToString();
+            TxtEngineType.Text = dataGridView1.Rows[choosen].Cells[3].Value.ToString();
+            TxtStatus.Text = dataGridView1.Rows[choosen].Cells[4].Value.ToString();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            linking.Open();
+            OleDbCommand order = new OleDbCommand("update [Sayfa1$] set Model=@p2,BodyType=@p3,EngineType=@p4 where Year=@p1", linking);
+            
+            order.Parameters.AddWithValue("@p2", TxtModel.Text);
+            order.Parameters.AddWithValue("@p3", TxtBodyType.Text);
+            order.Parameters.AddWithValue("@p4", TxtEngineType.Text);
+            order.Parameters.AddWithValue("@p5", TxtStatus.Text);
+            order.Parameters.AddWithValue("@p1", TxtYear.Text);
+            order.ExecuteNonQuery();
+            linking.Close();
+            MessageBox.Show("Information of vehicle has been updated", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Data();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void TxtStatus_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
